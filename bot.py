b0299efa0a2aaffc28a7b0a0c7d1793056586dae
Petersproff <peterspros7@gmail.com>
2026-01-11 14:29:49 +0100

    Add NOWPAY_API_KEY environment variable

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+NOWPAY_API_KEY = os.getenv("NOWPAY_API_KEY")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
